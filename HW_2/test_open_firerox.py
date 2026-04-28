35e18fecbd37dea53119acac0959b337e4be7aa6
Alina_Rytska <alinarytskade@gmail.com>
2026-04-28 21:20:02 +0200

    Add HW_2

---
+++ HW_2/test_open_firerox.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+
+    yield driver
+
+    driver.quit()
+
+
+def test_payment_methods_section(driver):
+    driver.get("https://itcareerhub.de/ru/")
+    sleep(5)
+
+    payment_link = driver.find_element(By.LINK_TEXT, "Способы оплаты")
+    payment_link.click()
+    sleep(5)
+
+    payment_section = driver.find_element(By.CSS_SELECTOR, "#rec1921734713 > div > div > div.t396__filter")
+    payment_section.screenshot("./payment_methods_section.png")
